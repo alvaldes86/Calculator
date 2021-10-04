2f619ca64d462dc1af9936488f0aa503e00aa615
alvaldes86 <76670909+alvaldes86@users.noreply.github.com>
2021-10-04 16:53:15 -0400

    Change variable storage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*========Global Variables=========*/
+
+let firstNumber = [];
+let secondNumber = [];
+let operator = "";
+let dot = document.querySelector(".dot").innerHTML;
+
+
+
 /*=======EVENTS===========*/
 window.addEventListener('load', ()=>{
     clearScreen();
@@ -9,41 +18,54 @@ clear.addEventListener('click', ()=>{
 })
 
 
+
+
+
 const numbers = document.querySelectorAll(".number");   //return a node list from all the html elements that has the .number class
-let numberString = " ";
 numbers.forEach(function(button){   //forEach will call a funtion once for each of the elements in the array or node list
     button.addEventListener('click', ()=>{      //addEventListener will take two arguments the trigger event and a function
-        numberString = numberString + button.innerHTML;
-       update(numberString);                            //in this case console.log the clicked button
+        firstNumber.push(parseInt(button.innerHTML))
+        console.log(firstNumber)          
     });
 });
+
+
 
 const operators = document.querySelectorAll(".operator");  
 operators.forEach(function(operant){  
     operant.addEventListener('click', ()=>{    
-         console.log(operant.innerHTML)                         
+           operator = operant;
+           firstNumber = [];
+           console.log(firstNumber)
+           secondNumber = firstNumber;
+           console.log('this is first ' + firstNumber)
+           console.log('this is the second' + secondNumber) 
+          
     });
 });
 
+
 const equals = document.querySelector(".equals");  
-    equals.addEventListener('click', ()=>{    
+    equals.addEventListener('click', ()=>{
+        console.log('first ' + firstNumber, 'second ' + secondNumber)
     });
 
 
 
 
-/*============FUNCTIONS=================*/
- function update(numberString){
-    document.querySelector(".sub-screen-1").innerHTML = numberString;
-};
+/*============FUNCTIONS=================
+ function update(userInput){
+    document.querySelector(".sub-screen-1").innerHTML = userInput;
+    };*/
 
 function clearScreen(){
+    userInput = " ";
     document.querySelector(".sub-screen-1").innerHTML = " ";
     document.querySelector(".sub-screen-2").innerHTML = " ";
     document.querySelector(".total").innerHTML = " ";
  }; 
 
-function addition(){
-
+function addition(firstNumber, userInput){
+    return firstNumber + userInput;
 };
 
