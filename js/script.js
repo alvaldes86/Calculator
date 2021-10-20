176656afd4b7f05caac1466a04fe4a9d52f1bb18
alvaldes86 <76670909+alvaldes86@users.noreply.github.com>
2021-10-20 19:52:02 -0400

    refractoring

--- js/script.js
+++ js/script.js
@@ -5,6 +5,45 @@ let num2 = [];
 let operator = undefined;
 let total = [];
 
+/*============FUNCTIONS=================
+ function update(userInput){
+    document.querySelector(".sub-screen-1").innerHTML = userInput;
+    };*/
+
+    function clearScreen(){
+        num1 = [];
+        num2 = [];
+        operator = undefined;
+        document.querySelector(".sub-screen-1").innerHTML = "";
+        document.querySelector(".sub-screen-2").innerHTML = "";
+        document.querySelector(".total").innerHTML = "";
+     }; 
+    
+     function updateView(){
+        let x = num1.join("");
+        let y = num2.join("");
+        let result = total
+        document.querySelector(".sub-screen-1").innerHTML = x;
+        document.querySelector(".sub-screen-2").innerHTML = y;
+        document.querySelector(".total").innerHTML = result; 
+     }
+    
+    function addition(){
+        total = parseInt(num1.join("")) + parseInt(num2.join(""));
+        return total
+    };
+    
+    function sustraction(){
+        total = parseInt(num1.join("")) - parseInt(num2.join(""));
+    };
+    
+    function multiply(){
+        total = parseInt(num1.join("")) * parseInt(num2.join(""));
+    };
+    
+    function divide(){
+        total = parseInt(num1.join("")) / parseInt(num2.join(""));
+    };
 
 /*=======EVENTS===========*/
 window.addEventListener('load', ()=>{
@@ -15,26 +54,6 @@ const clear = document.querySelector(".clear");
 clear.addEventListener('click', ()=>{
     clearScreen();
 })
-
-const numbers = document.querySelectorAll(".number");   //return a node list from all the html elements that has the .number class
-numbers.forEach(function(button){   //forEach will call a funtion once for each of the elements in the array or node list
-    button.addEventListener('click', ()=>{              //addEventListener will take two arguments the trigger event and a function
-        
-        if(operator === undefined){
-            num1.push(parseInt(button.innerHTML))
-        }else{
-            num2.push(parseInt(button.innerHTML))
-        }
-        updateView();
-    });
-});
-
-const operators = document.querySelectorAll(".operator");  
-operators.forEach(function(operant){  
-    operant.addEventListener('click', ()=>{    
-            operator = operant.innerHTML
-    });
-});
 
 const equals = document.querySelector(".equals");  
     equals.addEventListener('click', ()=>{        
@@ -70,41 +89,26 @@ del.addEventListener('click', function(button){
             updateView();
         }
 });
-/*============FUNCTIONS=================
- function update(userInput){
-    document.querySelector(".sub-screen-1").innerHTML = userInput;
-    };*/
 
-function clearScreen(){
-    num1 = [];
-    num2 = [];
-    operator = undefined;
-    document.querySelector(".sub-screen-1").innerHTML = "";
-    document.querySelector(".sub-screen-2").innerHTML = "";
-    document.querySelector(".total").innerHTML = "";
- }; 
+const numbers = document.querySelectorAll(".number");   //return a node list from all the html elements that has the .number class
+numbers.forEach(function(button){   //forEach will call a funtion once for each of the elements in the array or node list
+    button.addEventListener('click', ()=>{              //addEventListener will take two arguments the trigger event and a function
+        
+        if(operator === undefined){
+            num1.push(parseInt(button.innerHTML))
+        }else{
+            num2.push(parseInt(button.innerHTML))
+        }
+        updateView();
+    });
+});
 
- function updateView(){
-    let x = num1.join("");
-    let y = num2.join("");
-    let result = total
-    document.querySelector(".sub-screen-1").innerHTML = x;
-    document.querySelector(".sub-screen-2").innerHTML = y;
-    document.querySelector(".total").innerHTML = result; 
- }
+const operators = document.querySelectorAll(".operator");  
+operators.forEach(function(operant){  
+    operant.addEventListener('click', ()=>{    
+            operator = operant.innerHTML
+            console.log(operator.length)
+    });
+});
 
-function addition(){
-    total = parseInt(num1.join("")) + parseInt(num2.join(""));
-};
 
-function sustraction(){
-    total = parseInt(num1.join("")) - parseInt(num2.join(""));
-};
-
-function multiply(){
-    total = parseInt(num1.join("")) * parseInt(num2.join(""));
-};
-
-function divide(){
-    total = parseInt(num1.join("")) / parseInt(num2.join(""));
-};
